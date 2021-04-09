feea481299587fa198dd2d89b9a31ae7c06032e7
neonSpork <leiknes@gmail.com>
2021-04-10 00:18:16 +0200

    fix: WIP basic framework

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def default():
+    temp = 10;
+    return render_template('default.html', temp=temp)
+
+@app.route('/secret')
+def secret():
+    return render_template('secret.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
