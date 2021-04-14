f9a2a939660aadbd31998f81969364e9a0af5b2e
neonSpork <leiknes@gmail.com>
2021-04-15 01:46:50 +0200

    fix: add missing imports

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for
-import time
+import RPi.GPIO as GPIO
+from hx711 import HX711
+from w1thermsensor import W1ThermSensor
 
 app = Flask(__name__)
 
@@ -59,3 +61,4 @@ def endPour():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    GPIO.cleanup()
