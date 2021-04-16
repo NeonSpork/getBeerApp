99036e5cdcca33fbc9c592c6facd7e84cd91ad93
neonSpork <leiknes@gmail.com>
2021-04-16 13:50:24 +0200

    fix: generalize kiosk script

--- kiosk.sh
+++ kiosk.sh
@@ -6,8 +6,8 @@ xset -dpms
 
 unclutter -root &
 
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
 
-$sudo python3 /home/pi/getBeerApp/app.py
+$sudo python3 ~/getBeerApp/app.py
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:5000
