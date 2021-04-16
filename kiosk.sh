b34d97ae5c02b4832682120e7ca1dd8a0c2afffa
neonSpork <leiknes@gmail.com>
2021-04-16 11:29:36 +0200

    fix: automate starting flask

--- kiosk.sh
+++ kiosk.sh
@@ -9,4 +9,5 @@ unclutter -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
+$python3 /home/pi/getBeerApp/app.py
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:5000
