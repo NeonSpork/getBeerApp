5161b677e96506f28fc3c05ec2c41c5fd78cc7f8
neonSpork <leiknes@gmail.com>
2021-04-18 18:08:21 +0200

    fix: correct path

--- kiosk.sh
+++ kiosk.sh
@@ -9,5 +9,5 @@ unclutter -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
 
-$sudo python3 ~/getBeerApp/app.py
+$sudo python3 /home/linaro/getBeerApp/app.py
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:5000
