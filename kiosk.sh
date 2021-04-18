cb1877f3eeb640b78a2a567dc7e1d97f12dbaa56
neonSpork <leiknes@gmail.com>
2021-04-18 18:22:38 +0200

    fix: correct path

--- kiosk.sh
+++ kiosk.sh
@@ -6,8 +6,8 @@ xset -dpms
 
 unclutter -root &
 
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/linaro/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/linaro/.config/chromium/Default/Preferences
 
 /usr/bin/bash start_server.sh
 /usr/bin/chromium --noerrdialogs --disable-infobars --kiosk http://localhost:5000
