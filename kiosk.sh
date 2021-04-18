b71f22961412b1bb81603d122b73f4162b375745
neonSpork <leiknes@gmail.com>
2021-04-18 18:17:00 +0200

    chore move python call to another shell script

--- kiosk.sh
+++ kiosk.sh
@@ -9,5 +9,5 @@ unclutter -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
 
-python3 app.py &
+sh start_server.sh
 /usr/bin/chromium --noerrdialogs --disable-infobars --kiosk http://localhost:5000
