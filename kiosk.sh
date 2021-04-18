8483d2cbd341053f34a7732f35638a3c1de63ec0
neonSpork <leiknes@gmail.com>
2021-04-18 18:21:06 +0200

    fix: shell improvements

--- kiosk.sh
+++ kiosk.sh
@@ -9,5 +9,5 @@ unclutter -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
 
-sh start_server.sh
+/usr/bin/bash start_server.sh
 /usr/bin/chromium --noerrdialogs --disable-infobars --kiosk http://localhost:5000
