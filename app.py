b189812cacfa889ff7530ba4f096409c91b45de6
neonSpork <leiknes@gmail.com>
2021-04-20 11:44:13 +0200

    refactor: exception handling, works with or without SBC

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, url_for
-import ASUS.GPIO as GPIO
-from hx711 import HX711
-from w1thermsensor import W1ThermSensor
-import time
+try:
+    import ASUS.GPIO as GPIO
+    from hx711 import HX711
+    from w1thermsensor import W1ThermSensor
+except:
+    print("No compatible SBC detected!")
+    print("ASUS.GPIO, hx711, w1thermsensor are NOT imported.")
 
 app = Flask(__name__)
 
 # TODO add custom name - requires messing about with /etc/hosts and adding an alias
 # app.config['SERVER_NAME'] = 'getbeer:5000'
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.IN)  # Temp sensor DS18B20
-GPIO.setup(3, GPIO.IN)  # HX711 load sensor DT
-GPIO.setup(5, GPIO.IN)  # HX711 load sensor SDK
 beerPin = 37
 secretPin = 38
-GPIO.setup(beerPin, GPIO.OUT)  # Output pin to solenoid BEER valve
-GPIO.setup(secretPin, GPIO.OUT)  # Output pin to solenoid VODKA valve
+try:
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.IN)  # Temp sensor DS18B20
+    GPIO.setup(3, GPIO.IN)  # HX711 load sensor DT
+    GPIO.setup(5, GPIO.IN)  # HX711 load sensor SDK
+    GPIO.setup(beerPin, GPIO.OUT)  # Output pin to solenoid BEER valve
+    GPIO.setup(secretPin, GPIO.OUT)  # Output pin to solenoid VODKA valve
+except:
+    class GPIO():
+        def output(pin, status):
+            print("Simulating gpio{}, status: {}".format(pin, status))
+        HIGH = "GPIO.HIGH"
+        LOW = "GPIO.LOW"
+        def cleanup():
+            print("Simulating GPIO.cleanup()")
 
 try:
     hx = HX711(dout=3, pd_sck=5)
@@ -39,8 +51,7 @@ def getTemp():
     try:
         temp = sensor.get_temperature()
     except Exception as e:
-        temp = e.__name__
-    # temp = "{}".format(time.localtime().tm_sec)
+        temp = e
     return temp
 
 @app.route('/getPints')
@@ -50,9 +61,8 @@ def getPints():
         pints = int((grams - 4250)*0.5)  # dry weight of keg is ca. 4250g
         if pints < 0:
             pints = 0
-    except Exception as e:
-        pints = e
-    # pints = "{}".format(time.localtime().tm_min)
+    except:
+        pints = "n/a"
     return pints
 
 @app.route('/', methods= ['GET'])
